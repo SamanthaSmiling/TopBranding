d74c74079bbc7ce0b3bf070b93402b78ba3817e9
Samantha <sammyissmiling@gmail.com>
2024-09-14 14:48:39 -0400

    update

--- test_app.py
+++ test_app.py
@@ -1,12 +1,14 @@
+"""
+Test goes here
+"""
 import pytest
 from brand_glimpse import read_file, glimpse
 
-@pytest.fixture
-def client():
-    read_file.config['data/transaction_data.csv'] = True
-    with read_file.test_client() as client:
-        yield client
 
-def test_home(client):
-    rv = client.get('/')
-    assert rv.status_code == 200
+
+
+
+def test_func():
+    assert read_file("data/transaction_data.csv") == True
+    print("You are Reading correctly")
+
